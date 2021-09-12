ade343f943207e1a57e245f76608ad769fd81969
Max <voronin.maksim@mail.com>
2021-09-12 22:47:14 +0500

    add tour, departure, index logic

--- tours/urls.py
+++ tours/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import MainView, TourView, DepartureView
+from .views import MainView, TestView, TourView, DepartureView
 
 
 urlpatterns = [
-    path('', MainView.as_view(), name="main"),
+    path('', MainView.as_view(), name="index"),
     path('tour/<int:id>/', TourView.as_view(), name="tour"),
-    path('departure/<str:departure>/', DepartureView.as_view(), name="departure")
+    path('departure/<str:departure>/', DepartureView.as_view(), name="departure"),
 ]
--- tours/views.py
+++ tours/views.py
@@ -1,20 +1,56 @@
+from random import shuffle
+
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+from django.urls import reverse
+from data import tours, departures
 
 
 class MainView(View):
 
     def get(self, request):
-        return render(request, 'tours/index.html')
+        random_tours = list(tours.items())
+        shuffle(random_tours)
+        random_tours = {key: value for key, value in random_tours[:6]}
+        context = {'tours': random_tours}
+        return render(request, 'tours/index.html', context=context)
 
 
 class DepartureView(View):
 
     def get(self, request, departure):
-        return render(request, 'tours/departure.html')
+        ru_departure = departures[departure]
+        filtered_tours = {key: value for key, value in list(tours.items()) if value['departure'] == departure}
+        tours_count = len(filtered_tours)
+        prices = [value['price'] for value in list(filtered_tours.values())]
+        min_price, max_price = min(prices), max(prices)
+        nigths = [value['nights'] for value in list(filtered_tours.values())]
+        min_night, max_night = min(nigths), max(nigths)
+
+        context = {'departure': ru_departure, 'tours': filtered_tours, 'min_price': min_price,
+                   'max_price': max_price, 'min_night': min_night, 'max_night': max_night,
+                   'tours_count': tours_count}
+        return render(request, 'tours/departure.html', context=context)
 
 
 class TourView(View):
 
     def get(self, request, id):
-        return render(request, 'tours/tour.html')
+        tour = tours[id]
+        stars = '★' * int(tour['stars'])
+        departure = departures[tour['departure']]
+        context = {'tour': tour, 'stars': stars, 'departure': departure}
+        return render(request, 'tours/tour.html', context=context)
+
+
+class TestView(View):
+
+    def get(self, request, year, **kwargs):
+        summary = kwargs.get('summary', False)
+        context = {'name': 'Max', 'cat': 'Phenix', 'summary': summary, 'year': year}
+        if year < 1900:
+            return HttpResponseRedirect(reverse('time-loop', args=[2000]))
+        elif year > 2000:
+            return HttpResponseRedirect(reverse('time-loop', kwargs={'year': 1900}))
+        return render(request, 'test.html', context=context)
